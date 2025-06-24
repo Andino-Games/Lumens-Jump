fe62881b8c5b2dbefe9452b8b5498bc93af56c98
CamiloGato <cm.andres022@hotmail.com>
2025-06-24 16:37:50 -0500

    Update: Finished PowerUp base components

---
+++ Assets/Systems/PowerUps/DemoPowerUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using Components;
+
+namespace Systems.PowerUps
+{
+    public class DemoPowerUp : PowerUpComponent
+    {
+        public override void SetUpComponents()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override IEnumerator Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
