78055ae3782d5ad77f1104ba5b89762cd26929b1
Jose-Agudelo-LITG <46972134+NothigBox@users.noreply.github.com>
2026-05-31 12:01:20 -0500

    Ads Configuration Fixed

--- Assets/Systems/Ads/AdsConfiguration.cs
+++ Assets/Systems/Ads/AdsConfiguration.cs
@@ -3,11 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "AdsConfiguration", menuName = "Scriptable Objects/AdsConfiguration")]
 public class AdsConfiguration : ScriptableObject
 {
-#if UNITY_ANDROID
-    [SerializeField] private string appKey;
-    [SerializeField] private string rewardedKey;
-    [SerializeField] private string interstitialKey;
-#elif UNITY_IOS
+#if UNITY_ANDROID || UNITY_IOS
     [SerializeField] private string appKey;
     [SerializeField] private string rewardedKey;
     [SerializeField] private string interstitialKey;
